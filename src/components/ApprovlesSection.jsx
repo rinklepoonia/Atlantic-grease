a557d1ece898150ad783ed85597f4323f4e5de7b
rinklepoonia <rinklepoonia889@gmail.com>
2024-05-23 16:50:06 +0530

    manage-spacing-in-page

--- src/components/ApprovlesSection.jsx
+++ src/components/ApprovlesSection.jsx
@@ -6,13 +6,14 @@ function ApprovlesSection() {
         <div id='testimonials' className='position-relative pb_lg_78 pb_10 pt-xl-0 pt-lg-4'>
             <div className='approvals_grident pos_blueGrident d-sm-block d-none'></div>
             <div className='max_w_1140 custom_container'>
-                <h3 className='ff_pop fw-800 text_52xl lh_62 color_white text-uppercase mb_lg_53 mb_20'>OEM  <span
-                    className='fw-normal ff_pop text_52xl lh_62 color_white style_italic bg_blue d-inline-block transform_skew p-2 btnn2'><span>Approvals</span></span></h3>
+                <div className='d-flex justify_content_center'>
+                    <h3 className='ff_pop fw-800 text_52xl lh_62 color_white text-uppercase mb_lg_53 mb_20'>OEM  <span
+                        className='fw-normal ff_pop text_52xl lh_62 color_white style_italic bg_blue d-inline-block transform_skew p-2 btnn2'><span>Approvals</span></span></h3>
+                </div>
             </div>
-            {/* <div className='max_w_1536 w-100 custom_container'> */}
-                <div className='mx-auto'>
+            <div className='mx-auto'>
                 <div className="row flex-xl-row flex-column-reverse">
-                    <div className="col-xl-8 col-12 approve_bg_img bg_no_repeat approvel_bg_cover h_417 py_lg_59_53 p_sm_30 p_20">
+                    <div className="col-xl-8 col-12 approve_bg_img bg_no_repeat approvel_bg_cover h_417 py_20 py_xl_59_53 p_sm_30 p_20">
                         <div className='ml_150 position-relative z-1 d-flex flex-column justify_content_center align_items_center'>
                             <p className='ff_dm fw-normal text_24_2md lh_lg_36 lh_26 max_w_567 mb-lg-3 mb-sm-2 mb-1'>At Atlantic Grease and Lubricants, we have <span className='fw-bold'>secured approvals</span> leading automobile manufacturers. Thus, we demonstrate our commitment to delivering the best possible outcomes to our customers.</p>
                             <p className='ff_dm fw-normal text_24_2md lh_lg_36 lh_26 max_w_567'>Recognising the <span className='fw-bold'>significance of quality</span> to our valued customers, we uphold the utmost standards of excellence in all our endeavors.</p>
--- src/components/ManufactorsSection.jsx
+++ src/components/ManufactorsSection.jsx
@@ -3,22 +3,20 @@ import ManufactorImg from '../assets/images/png/manufactor-img.png'
 
 function ManufactorsSection() {
     return (
-        <div id='services' className='py_lg_184_210 py_sm_90 py_30 position-relative'>
+        <div id='services' className='py_xl_184_210 py_sm_90 py_40 position-relative'>
             <div className='manufactore_grident pos_grident_manufactor position-absolute d-sm-block d-none'></div>
-            {/* <div className='custom_container max_w_1230'> */}
             <div className='container'>
-                <div className='d-flex justify-content-xl-end'>
+                <div className='d-flex justify-content-xl-end justify_content_center'>
                     <h2 className='ff_pop fw-800 text_52xl lh_lg_52 lh_sm_45 lh_36 color_white max_w_745 text-uppercase'>Pioneers of <span className='ff_pop fw-800 text_52xl lh_lg_52 lh_sm_45 lh_36 color_white style_italic fw-normal bg_red d-inline-block transform_skew p-md-2 p-1 bttn'><span> Lubricants </span></span>
                         <span className='d-lg-block'>& Grease Manufacturing</span></h2>
                 </div>
             </div>
-            {/* <div className='max_w_1536 w-100 custom_container'> */}
             <div className='mx-auto'>
-                <div className="row">
+                <div className="row justify-content-center">
                     <div data-aos="fade-up" className="col-xl-5 col-10 d-flex justify_content_center">
                         <img className='position-relative w_100 max_w_600 top_-13' src={ManufactorImg} alt="ManufactorImg" />
                     </div>
-                    <div className="col-xl-7 white_bg bg_img_cover mt-lg-5 mt-md-3 mt-sm-1 mt-0 py_lg_68 py_sm_30 py_10 h_417">
+                    <div className="col-xl-7 white_bg bg_img_cover mt-xl-5 mt-0 py_lg_20 py_xl_68 py_sm_30 py_10 h_417">
                         <div className='d-flex justify-content-center'>
                             <p className='ff_dm fw-normal text_24_2md lh_36 max_w_556 color_black z-1 mb-xxl-5 mb-xl-4 mb-lg-3 mb-md-3 mb-1'>Atlantic Grease & Lubricants is one of the leading manufacturers in the <span className='ff_dm fw-bold'>automotive industry</span> providing high-performance products certified by renowned European and American automobile industries.</p>
                         </div>
